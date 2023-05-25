6637315cc1094b6154c287c216e010d6b76c164b
Munyaradzi Marinda <munyaradzimarinda2@gmail.com>
2023-05-25 15:12:14 +0200

    Removed Realm from packages.

--- App.js
+++ App.js
@@ -1,14 +1,13 @@
 import { StatusBar } from "expo-status-bar";
 import SAVE_LESS_WHATAPP_CHAT from "./src/app-screens/save_less_whatapp_chat/save_less_whatapp_chat.js";
-import { StyleSheet, View, Text } from "react-native";
-import Test from "./src/app-screens/test/test.js";
+import { StyleSheet, View, Text } from "react-native"; 
 
 export default function App() {
   return (
     <View style={styles.container}>
       <StatusBar backgroundColor={"white"} barStyle={"light-content"} />
-      {/* <SAVE_LESS_WHATAPP_CHAT /> */}
-      <Test />
+      <SAVE_LESS_WHATAPP_CHAT />
+      {/* <Test /> */}
     </View>
   );
 }
